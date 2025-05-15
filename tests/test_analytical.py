14be7cdec7af3157e437ca47137350157f9e77d0
patquem <patrick.quemere@cea.fr>
2025-05-15 18:56:10 +0200

    add assert len(shifts_cumul) == 60

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -7,4 +7,5 @@ from examples.example_analytical import ex_staggered_dots
 def test_staggered_dots():
     _, _, shifts_cumul = ex_staggered_dots()
 
+    assert len(shifts_cumul) == 60
     assert shifts_cumul[-1] == pytest.approx(np.array([175.60457583, 56.7499515]))
